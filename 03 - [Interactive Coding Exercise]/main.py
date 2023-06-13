6bf1384856f3e686b90f2b0478156c4bb900f211
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2023-06-13 15:22:49 -0300

    main.py

--- 03 - [Interactive Coding Exercise]/main.py
+++ 03 - [Interactive Coding Exercise]/main.py
@@ -9,12 +9,13 @@ print(f' {row1}\n {row2}\n {row3}')
 position = input(' Where do you want to put the treasure? ')
 # 🚨 Don't change the code above 👆🏻
 
-# Write your code below this line👇🏻
+# Write your code below this line 👇🏻
 horizonal = int(position[0])
 vertical = int(position[1])
 
 map[vertical - 1][horizonal - 1] = '🟥'
-# Write your code above this line👆🏻
+# Write your code above this line 👆🏻
 
 # 🚨 Don't change the code below 👇🏻
 print(f' {row1}\n {row2}\n {row3}')
+# 🚨 Don't change the acode above 👆🏻
